d495ef8320710900fdb8a865ce232941669be1ba
andreldgraciano <dedasgdias@gmail.com>
2024-11-06 13:21:38 -0300

    first commit

---
+++ src/javascript/script.js
@@ -0,0 +1,32 @@
+const images = [
+    // 'images/1.png',
+    'images/2.png',
+    'images/3.png',
+];
+let currentImageIndex = 0;
+
+const background = document.getElementById('background');
+
+// Função para trocar a imagem e aplicar a escala
+function changeBackground() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    if (currentImageIndex === 2) {
+        background.classList.remove('bg-center');
+        background.classList.add('bg-right');
+    }
+    if (currentImageIndex === 0) {
+        background.classList.remove('bg-right');
+        background.classList.add('bg-center');
+    }
+    background.style.backgroundImage = `url(${images[currentImageIndex]})`;
+
+    // Aplica a escala animada
+    background.classList.toggle('scale-100');
+    background.classList.toggle('scale-105');
+}
+
+// Executa a primeira troca de imagem ao carregar
+changeBackground();
+
+// Alterna a cada 5 segundos
+setInterval(changeBackground, 5000);
